1495ddb7c30cc674e4b558601118a711c3ae197e
Theofilos Papapanagiotou <theofilos.papapanagiotou@student.uva.nl>
2019-02-17 14:47:54 +0100

    complete shape by integration, columns/rows/avg

--- assignment_1/photometric/photometric_stereo.m
+++ assignment_1/photometric/photometric_stereo.m
@@ -6,7 +6,7 @@ disp('Part 1: Photometric Stereo')
 
 % obtain many images in a fixed view under different illumination
 disp('Loading images...')
-image_dir = './SphereGray5/';   % TODO: get the path of the script
+image_dir = './SphereGray25/';   % TODO: get the path of the script
 %image_ext = '*.png';
 
 [image_stack, scriptV] = load_syn_images(image_dir);
@@ -15,7 +15,7 @@ fprintf('Finish loading %d images.\n\n', n);
 
 % compute the surface gradient from the stack of imgs and light source mat
 disp('Computing surface albedo and normal map...')
-[albedo, normals] = estimate_alb_nrm(image_stack, scriptV, false);
+[albedo, normals] = estimate_alb_nrm(image_stack, scriptV, true);
 
 
 %% integrability check: is (dp / dy  -  dq / dx) ^ 2 small everywhere?
@@ -27,12 +27,12 @@ SE(SE <= threshold) = NaN; % for good visualization
 fprintf('Number of outliers: %d\n\n', sum(sum(SE > threshold)));
 
 %% compute the surface height
-height_map = construct_surface( p, q );
+height_map = construct_surface( p, q ,'average');
 
 %% Display
 show_results(albedo, normals, SE);
 show_model(albedo, height_map);
-
+%{
 
 %% Face
 [image_stack, scriptV] = load_face_images('./yaleB02/');
@@ -54,4 +54,4 @@ height_map = construct_surface( p, q );
 
 show_results(albedo, normals, SE);
 show_model(albedo, height_map);
-
+%}
