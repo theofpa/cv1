007fea57397f2e351e5035ba770368422f6ef36a
Niraj Sewgobind <nirajsewgobind@gmail.com>
2019-02-20 00:25:02 +0000

    Update rgb2normedrgb.m

--- assignment_1/colorspace/rgb2normedrgb.m
+++ assignment_1/colorspace/rgb2normedrgb.m
@@ -1,5 +1,18 @@
-function [output_image] = rgb2normedrgb(input_image)
+function [new_image] = rgb2normedrgb(input_image)
 % converts an RGB image into normalized rgb
+im = input_image;
 
+R = im(:,:,1);
+G = im(:,:,2);
+B = im(:,:,3);
+
+RGB = R+G+B;
+
+r = R./RGB;
+g = G./RGB;
+b = B./RGB;
+
+new_image(:,:,1) = r;
+new_image(:,:,2) = g;
+new_image(:,:,3) = b;
 end
-
