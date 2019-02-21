b34af7828a82e7a00545aa3767ed11b97e850a7c
Mehdi Gunes <amehdigunes@gmail.com>
2019-02-21 19:35:49 +0100

    Add Low-level filters - Gaussian filters - 2D Gaussian filter

--- assignment_2/Part-1_ Image Enhancement/gauss2D.m
+++ assignment_2/Part-1_ Image Enhancement/gauss2D.m
@@ -1,3 +1,11 @@
 function G = gauss2D( sigma , kernel_size )
     %% solution
+    G = zeros(1, kernel_size);
+    if mod(kernel_size, 2) == 0
+        error('kernel_size must be odd, otherwise the filter will not have a center to convolve on')
+    end
+    G_x = gauss1D(sigma, kernel_size);
+    G_y = gauss1D(sigma, kernel_size);
+    G = G_x .* G_y;
+    G = G ./ sum(G);
 end
