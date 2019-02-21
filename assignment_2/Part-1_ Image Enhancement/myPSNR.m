528b386b18ba5435865f961a52ab6fc3ed09f702
Mehdi Gunes <amehdigunes@gmail.com>
2019-02-21 20:34:44 +0100

    Add applications - image denoising - PSNR

--- assignment_2/Part-1_ Image Enhancement/myPSNR.m
+++ assignment_2/Part-1_ Image Enhancement/myPSNR.m
@@ -1,5 +1,8 @@
 function [ PSNR ] = myPSNR( orig_image, approx_image )
-fprintf('Not implemented\n')
-
+%fprintf('Not implemented\n')
+    [m, n] = size(orig_image);
+    I_max = max(orig_image, [], 'all');
+    RMSE = sqrt(1/(m*n) * sum((orig_image - approx_image).^2, 'all'));
+    PSNR = 20 * log10(I_max/RMSE)
 end
 
