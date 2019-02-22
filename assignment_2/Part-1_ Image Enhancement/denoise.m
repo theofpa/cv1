69572555f5f958f9f78705d0be39919c657ab491
Mehdi Gunes <amehdigunes@gmail.com>
2019-02-22 20:29:00 +0100

    Add applications - image denoising - denoise

--- assignment_2/Part-1_ Image Enhancement/denoise.m
+++ assignment_2/Part-1_ Image Enhancement/denoise.m
@@ -1,11 +1,21 @@
 function [ imOut ] = denoise( image, kernel_type, varargin)
-
 switch kernel_type
     case 'box'
-        fprintf('Not implemented\n')
+        if size(varargin) == [1, 1]
+            [kernelSize] = varargin{:};
+            imOut = imboxfilt(image, kernelSize);
+        else
+            imOut = imboxfilt(image);
+        end
     case 'median'
-        fprintf('Not implemented\n')
+        if size(varargin) == [1, 1]
+            [kernelSize] = varargin{:};
+            imOut = medfilt2(image, [kernelSize, kernelSize]);
+        else
+            imOut = medfilt2(image);
+        end
     case 'gaussian'
-        fprintf('Not implemented\n')
+        [sigma, kernelSize] = varargin{:};
+        imOut = imfilter(image ,gauss2D(sigma, kernelSize));
 end
 end
