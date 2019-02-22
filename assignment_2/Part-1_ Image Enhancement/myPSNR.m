a31917529e82bf26970b93ecaca74ec2bcfd5320
Mehdi Gunes <amehdigunes@gmail.com>
2019-02-22 19:15:01 +0100

    Fix applications - image denoising - PSNR

--- assignment_2/Part-1_ Image Enhancement/myPSNR.m
+++ assignment_2/Part-1_ Image Enhancement/myPSNR.m
@@ -1,5 +1,7 @@
 function [ PSNR ] = myPSNR( orig_image, approx_image )
 %fprintf('Not implemented\n')
+    orig_image = im2double(orig_image);
+    approx_image = im2double(approx_image);
     [m, n] = size(orig_image);
     I_max = max(orig_image, [], 'all');
     RMSE = sqrt(1/(m*n) * sum((orig_image - approx_image).^2, 'all'));
