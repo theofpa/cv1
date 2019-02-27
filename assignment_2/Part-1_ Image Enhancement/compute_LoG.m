af394d28b89f2d6d8e9ae55376d179cbb7a228ae
Niraj Sewgobind <nirajsewgobind@gmail.com>
2019-02-27 22:31:42 +0000

    Update compute_LoG.m

--- assignment_2/Part-1_ Image Enhancement/compute_LoG.m
+++ assignment_2/Part-1_ Image Enhancement/compute_LoG.m
@@ -3,16 +3,26 @@ function imOut = compute_LoG(image, LOG_type)
 switch LOG_type
     case 1
         %method 1
-        fprintf('Not implemented\n')
-
+        hsize = 5;
+        sigma = 0.5;
+        h = fspecial('gaussian', hsize, sigma);
+        filtered_image = imfilter(image, h, 'conv');
+        h2 = fspecial('laplacian');
+        imOut = imfilter(filtered_image, h2, 'conv');
     case 2
         %method 2
-        fprintf('Not implemented\n')
-
+        hsize = 5;
+        sigma = 0.5;
+        h = fspecial('log', hsize, sigma);
+        imOut = imfilter(image, h, 'conv');
     case 3
         %method 3
         fprintf('Not implemented\n')
 
 end
+
+figure;
+imshow(imOut);
+
 end
 
