686e4e5051bcedce52b68a5c967a5ef2fc6746f5
Niraj Sewgobind <nirajsewgobind@gmail.com>
2019-03-06 15:06:46 +0000

    Update lucas_kanade.m

--- assignment_3/lucas_kanade.m
+++ assignment_3/lucas_kanade.m
@@ -19,10 +19,30 @@ regions2 = divide_img(img2, max_cols, max_rows);
 
 
 A = calculate_A(regions1);
-A{1}
+A{1};
 
-%A_t = calculate_A_t(A);
+A_t = calculate_A_t(A);
+A_t{1};
 
+b = calculate_b(regions1, regions2);
+b{60};
+
+v = calculate_v(A, A_t, b);
+v{60};
+
+draw_img(i1, v);
+end
+
+function draw_img(i1, v)
+figure;
+imshow(i1);
+hold on;
+
+for x = 1:length(v)
+    v1 = v{x}(1:15, :);
+    v2 = v{x}(16:30, :);
+    
+end
 end
 
 function [divided_img] = divide_img(img, cols, rows)
@@ -46,14 +66,29 @@ function [A] = calculate_A(regions)
 A = {};
 for x = 1:length(regions)
     [I_x, I_y] = gradient(regions{x});
-    A{x} = {I_x,I_y};
+    A{x} = [I_x,I_y];
 end
 end
 
-function [A_t] = calculate_A_t(As)
+function [A_t] = calculate_A_t(A)
 A_t = {};
-for x = 1:length(As)
-    
+for x = 1:length(A)
+    A_t{x} = A{x}';
+end
 end
 
+function [b] = calculate_b(region1, region2)
+b = {};
+for x = 1:length(region1)
+    b{x} = region1{x}-region2{x};
+end
+end
+
+function [v] = calculate_v(A, A_t, b)
+v = {};
+for x = 1:length(A)
+    r = A_t{x}*A{x};
+    y = A_t{x}*b{x}; 
+    v{x} = r\y; 
+end
 end
