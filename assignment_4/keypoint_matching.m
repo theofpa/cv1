73678087238321863a21a9bdafa230408289aed4
Haroen Bashir <hqbashir@gmail.com>
2019-03-13 14:44:02 +0000

    Update keypoint_matching.m

--- assignment_4/keypoint_matching.m
+++ assignment_4/keypoint_matching.m
@@ -9,11 +9,6 @@ function [matches] = keypoint_matching(im1,im2)
 %This function returns the supposed matches between region descriptors in
 %each image. 
 
-%Load imgs
-img1 = imread(im1);
-img2 = imread(im2);
-i1 = im2single(img1);
-i2 = im2single(img2);
 
 %%Implementing David Lowe's SIFT functions to extract and match
 %%descriptors%%
@@ -22,23 +17,23 @@ i2 = im2single(img2);
 threshold = 30; % Threshold for ubcmatch algorithm. higher is less keypoints. 
 
 %Find Matches
-[f1, d1] = vl_sift(i1) ;
-[f2, d2] = vl_sift(i2) ;
+[f1, d1] = vl_sift(im1) ;
+[f2, d2] = vl_sift(im2) ;
 [matches, scores] = vl_ubcmatch(d1, d2, threshold) ; %Index of matches and distance between scores
 
 %Show images
 figure; 
-imshow(cat(2, img1, img2)) ;
+imshow(cat(2, im1, im2)) ;
 
 %Determine coordinate vectors for lines
 x1 = f1(1,matches(1,:)) ;
-x2 = f2(1,matches(2,:)) + size(i1,2) ;
+x2 = f2(1,matches(2,:)) + size(im1,2) ;
 y1 = f1(2,matches(1,:)) ;
 y2 = f2(2,matches(2,:)) ;
 
 % Plot keypoints
 vl_plotframe(f1(:,matches(1,:))) ;
-f2(1,:) = f2(1,:) + size(i1,2) ;
+f2(1,:) = f2(1,:) + size(im1,2) ;
 vl_plotframe(f2(:,matches(2,:))) ;
 
 % Plot lines
