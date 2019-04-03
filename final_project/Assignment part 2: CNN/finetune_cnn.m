6afb51a5171eadeb4eeb1d929f6e0705c8c67e77
Haroen Bashir <hqbashir@gmail.com>
2019-04-03 20:36:15 +0000

    Update finetune_cnn.m

--- final_project/Assignment part 2: CNN/finetune_cnn.m
+++ final_project/Assignment part 2: CNN/finetune_cnn.m
@@ -1,15 +1,16 @@
 classdef finetune_cnn < handle
-    
-    properties
-    end
-    
-    methods(Static)
+   
+   properties
+   end
+   
+   methods(Static)
 
 function [net, info, expdir] = finetune_cnn(varargin)
 
 %% Define options
 run(fullfile(fileparts(mfilename('fullpath')), ...
-  '..', '..', '..', 'matlab', 'vl_setupnn.m')) ;
+ 'libs' , 'matconvnet-1.0-beta25', 'matlab', 'vl_setupnn.m')) ;
+
 
 opts.modelType = 'lenet' ;
 [opts, varargin] = vl_argparse(opts, varargin) ;
@@ -27,7 +28,7 @@ opts.train = struct() ;
 opts = vl_argparse(opts, varargin) ;
 if ~isfield(opts.train, 'gpus'), opts.train.gpus = []; end;
 
-opts.train.gpus = [0];
+opts.train.gpus = []; %[0]; % GPU
 
 
 
@@ -37,31 +38,28 @@ net = update_model();
 
 %% TODO: Implement getIMDB function below
 
-if exist(opts.imdbPath, 'file')
+%if exist(opts.imdbPath, 'file')
   imdb = load(opts.imdbPath) ;
-else
-  imdb = getIMDB() ;
-  mkdir(opts.expDir) ;
-  save(opts.imdbPath, '-struct', 'imdb') ;
-end
+%else
+%  imdb = getIMDB() ;
+%  mkdir(opts.expDir) ;
+%  save(opts.imdbPath, '-struct', 'imdb') ;
+%end
 
 %%
-net.meta.classes.name = imdb.meta.classes(:)' ;
+net.meta.classes.name = imdb.imdb.meta.classes(:)' ;
 
+function [images, labels] = getSimpleNNBatch(imdb, batch)
 % -------------------------------------------------------------------------
-%                                                                     Train
-% -------------------------------------------------------------------------
+images = imdb.images.data(:,:,:,batch) ;
+disp(batch);
+labels = imdb.images.labels(1,batch) ;
+% labels = imdb.images.labels(batch,1) ;
 
-trainfn = @cnn_train ;
-[net, info] = trainfn(net, imdb, getBatch(opts), ...
-  'expDir', opts.expDir, ...
-  net.meta.trainOpts, ...
-  opts.train, ...
-  'val', find(imdb.images.set == 2)) ;
+if rand > 0.5, images=fliplr(images) ; end
 
-expdir = opts.expDir;
 end
-% -------------------------------------------------------------------------
+
 function fn = getBatch(opts)
 % -------------------------------------------------------------------------
 switch lower(opts.networkType)
@@ -73,14 +71,40 @@ switch lower(opts.networkType)
 end
 
 end
-
-function [images, labels] = getSimpleNNBatch(imdb, batch)
 % -------------------------------------------------------------------------
-images = imdb.images.data(:,:,:,batch) ;
-labels = imdb.images.labels(1,batch) ;
-if rand > 0.5, images=fliplr(images) ; end
+%                                                                     Train
+% -------------------------------------------------------------------------
 
+trainfn = @cnn_train ;
+[net, info] = trainfn(net, imdb.imdb, getBatch(opts), ...
+  'expDir', opts.expDir, ...
+  net.meta.trainOpts, ...
+  opts.train, ...
+  'val', find(imdb.imdb.images.set == 2)) ;
+
+expdir = opts.expDir;
 end
+% -------------------------------------------------------------------------
+% function fn = getBatch(opts)
+% % -------------------------------------------------------------------------
+% switch lower(opts.networkType)
+%   case 'simplenn'
+%     fn = @(x,y) getSimpleNNBatch(x,y) ;
+%   case 'dagnn'
+%     bopts = struct('numGpus', numel(opts.train.gpus)) ;
+%     fn = @(x,y) getDagNNBatch(bopts,x,y) ;
+% end
+% 
+% end
+
+% function [images, labels] = getSimpleNNBatch(imdb, batch)
+% % -------------------------------------------------------------------------
+% images = imdb.images.data(:,:,:,batch) ;
+% disp(batch);
+% labels = imdb.images.labels(1,batch) ;
+% if rand > 0.5, images=fliplr(images) ; end
+% 
+% end
 
 % -------------------------------------------------------------------------
 function [imdb] = getIMDB()
@@ -88,15 +112,16 @@ function [imdb] = getIMDB()
 load('train.mat')
 TrainX = X;
 TrainY = y;
-TrainSet = uint8(ones(length(TrainX)));
+TrainSet = single(ones(length(TrainY)));
 load('test.mat')
 TestX = X;
 TestY = y;
-TestSet = uint8(2*ones(length(TestX)));
+TestSet = single(2*ones(length(TestY)));
 
 x = [TrainX;TestX];
 y = [TrainY;TestY];
-type = [TrainSet;TestSet];
+type = [TrainSet(1,:),TestSet(1,:)];
+disp(type(1:10))
 % ------------------------------------------------------------------------
 % Preapre the imdb structure, returns image data with mean image subtracted
 classes = {'airplanes', 'birds', 'ships', 'horses', 'cars'};
@@ -127,7 +152,9 @@ for i = 1:length(y)
    if index(i,1) == 0
        New_X(i,:) = x(i,:);
        New_Y(i,:) = y(i,:);
-       New_Set(i,:) = type(i,:);
+       disp(type(i)) 
+       disp(i)
+       New_Set(i) = type(i);
    end
 end
 
@@ -135,10 +162,15 @@ New_X(~any(New_X,2),:) = [];
 New_Y(~any(New_Y,2),:) = [];
 New_Set(~any(New_Set,2),:) = [];
 
+
 %Pre allocation
-data = uint8(zeros(32,32,3,length(New_Y)));
-labels = uint8(zeros(length(New_Y),1));
-set = uint8(zeros(length(New_Y),1));
+data = single(zeros(32,32,3,length(New_Y)));
+% labels = single(zeros(length(New_Y),1));
+labels = single(zeros(1,length(New_Y)));
+
+% set = single(zeros(length(New_Y),1));
+set = single(zeros(1,length(New_Y)));
+
 for i = 1 : length(New_Y)
     % Handle images
     img = reshape(New_X(i,:),[96,96,3]);
@@ -146,23 +178,30 @@ for i = 1 : length(New_Y)
     data(:,:,:,i) = img;
     % Handle labels
     if New_Y(i,1) == 3
-        labels(i,1) = 5;
+%         labels(i,1) = 5;
+        labels(1,i) = 5;
     elseif New_Y(i,1) == 9
-        labels(i,1) = 3;
+%         labels(i,1) = 3;
+        labels(1,i) = 3;
     elseif New_Y(i,1) == 7
-        labels(i,1) = 4;
+%         labels(i,1) = 4;
+        labels(1,i) = 4;
+
     else
-        labels(i,1) = New_Y(i,1);
+%         labels(i,1) = New_Y(i,1);
+        labels(1,i) = New_Y(i,1);      
     end
     % Handle sets
-    set(i,1) = New_Set(i);
+%     set(i,1) = New_Set(i);
+    set(1,i) = New_Set(i);
+
 end
 
 
 
 %%
 % subtract mean
-dataMean = uint8(mean(data(:, :, :, set == 1), 4));
+dataMean = single(mean(data(:, :, :, set == 1), 4));
 data = bsxfun(@minus, data, dataMean);
 
 imdb.images.data = data ;
